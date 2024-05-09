27f2646243575cd729c152600a6e51e05e885896
huybx2 <huybx2@ncb-bank.vn>
2024-05-10 00:12:44 +0700

    Init Project

--- HopsterPoke/GameScene.swift
+++ HopsterPoke/GameScene.swift
@@ -10,79 +10,56 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    // Background nodes
+    var background1: SKSpriteNode!
+    var background2: SKSpriteNode!
+    var poke: SKSpriteNode!
+    
+    // Speed of scrolling
+    let scrollSpeed: CGFloat = 2
     
     override func didMove(to view: SKView) {
+        // Set up backgrounds
+        background1 = createBackground()
+        background2 = createBackground()
+        poke = createPoke()
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        // Position background nodes
+        background1.position = CGPoint(x: 0, y: -frame.size.height/2 + 175)
+        background2.position = CGPoint(x: background1.size.width, y: -frame.size.height/2 + 175)
+        poke.position = CGPoint(x: -frame.width/2+135, y: -frame.size.height/2 + 400)
+        // Add background nodes to the scene
+        addChild(background1)
+        addChild(background2)
+        addChild(poke)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+//        // Move backgrounds
+        background1.position.x -= scrollSpeed
+        background2.position.x -= scrollSpeed
+        
+        // If a background node moves completely off the screen, move it to the other side
+        if background1.position.x < -background1.size.width {
+            background1.position.x = background2.position.x + background2.size.width
+        }
+        if background2.position.x < -background2.size.width {
+            background2.position.x = background1.position.x + background1.size.width
+        }
+    }
+    
+    func createPoke() -> SKSpriteNode {
+        let poke = SKSpriteNode(imageNamed: "rabbit_go_ahead")
+        poke.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        poke.size = CGSize(width: 110, height: 110)
+        return poke
+    }
+     
+    // Function to create a repeating background node
+    func createBackground() -> SKSpriteNode {
+        let background = SKSpriteNode(imageNamed: "underground_background")
+        background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        background.size = CGSize(width: 1717, height: 350)
+        return background
     }
 }
